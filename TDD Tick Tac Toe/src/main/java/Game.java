af46dffde79370a7538bdea04f0135a29fa7b21f
snowron <snowronark@gmail.com>
2020-09-27 13:20:17 +0300

    Fifth assigment review

--- TDD Tick Tac Toe/src/main/java/Game.java
+++ TDD Tick Tac Toe/src/main/java/Game.java
@@ -1,13 +1,11 @@
-import lombok.Getter;
 
-@Getter
 public class Game {
-    Player playerOne;
-    Player playerTwo;
-    String[][] board = new String[3][3];
-    String lastPlayedSign = "";
-    String winner;
-    int moveCount;
+    private final Player playerOne;
+    private final Player playerTwo;
+    private char[][] board = new char[3][3];
+    private char lastPlayedSign;
+    private String winner;
+    private int moveCount;
 
 
     public Game(Player playerOne, Player playerTwo) {
@@ -17,13 +15,12 @@ public class Game {
 
     public void playerOneMove(int horizontal, int vertical) {
         checkIsValid(vertical, horizontal);
-        move(horizontal, vertical, "X");
+        move(horizontal, vertical, 'X');
     }
 
     public void playerTwoMove(int horizontal, int vertical) {
         checkIsValid(horizontal, vertical);
-        move(horizontal, vertical, "O");
-
+        move(horizontal, vertical, 'O');
     }
 
     public void checkIsValid(int horizontal, int vertical) {
@@ -32,11 +29,11 @@ public class Game {
         }
     }
 
-    public void move(int horizontal, int vertical, String sign) {
-        if (this.lastPlayedSign.equals(sign)) {
+    public void move(int horizontal, int vertical, char sign) {
+        if (this.lastPlayedSign == sign) {
             throw new UnsupportedOperationException("Opponent turn, please wait yours.");
         }
-        if (!String.valueOf(this.board[horizontal][vertical]).equals("null")) {
+        if (this.board[horizontal][vertical] != '\u0000') {
             throw new UnsupportedOperationException("Please pick empty area.");
         }
         this.lastPlayedSign = sign;
@@ -49,44 +46,44 @@ public class Game {
     public void checkBoard() {
         try {
             for (int i = 0; i < 3; i++) {
-                if (this.board[i][0].charAt(0) == this.board[i][1].charAt(0) &&
-                        this.board[i][0].charAt(0) == this.board[i][2].charAt(0)) {
+                if (this.board[i][0] == this.board[i][1] &&
+                        this.board[i][0] == this.board[i][2]) {
                     this.getWinnerNameAndSign(this.board[i][0]);
                 }
-                if (this.board[0][i].charAt(0) == this.board[1][i].charAt(0) &&
-                        this.board[0][i].charAt(0) == this.board[2][i].charAt(0)) {
+                if (this.board[0][i] == this.board[1][i] &&
+                        this.board[0][i] == this.board[2][i]) {
                     this.getWinnerNameAndSign(this.board[0][i]);
                 }
             }
-            if (this.board[0][0].charAt(0) == this.board[1][1].charAt(0) &&
-                    this.board[0][1].charAt(0) == this.board[2][2].charAt(0)) {
+            if (this.board[0][0] == this.board[1][1] &&
+                    this.board[0][1] == this.board[2][2]) {
                 this.getWinnerNameAndSign(this.board[0][0]);
             }
-            if (this.board[0][2].charAt(0) == this.board[1][1].charAt(0) &&
-                    this.board[0][2].charAt(0) == this.board[2][0].charAt(0)) {
+            if (this.board[0][2] == this.board[1][1] &&
+                    this.board[0][2] == this.board[2][0]) {
                 this.getWinnerNameAndSign(this.board[0][2]);
             }
             if (this.moveCount == 9) {
-                this.getWinnerNameAndSign("XOX");
+                this.getWinnerNameAndSign('=');
             }
         } catch (Exception e) {
 
         }
     }
 
-    public void getWinnerNameAndSign(String sign) {
+    public void getWinnerNameAndSign(char sign) {
         switch (sign) {
-            case "X":
+            case 'X':
                 System.out.println("The player as named " + this.playerOne.name + " with that sign => " + sign);
                 this.winner = this.playerOne.name;
                 this.clear();
                 break;
-            case "O":
+            case 'O':
                 System.out.println("The player as named " + this.playerTwo.name + " with that sign => " + sign);
                 this.winner = this.playerTwo.name;
                 this.clear();
                 break;
-            case "XOX":
+            case '=':
                 System.out.println("The game is draw");
                 this.winner = "draw";
                 this.clear();
@@ -95,6 +92,13 @@ public class Game {
     }
 
     public void clear() {
-        this.board = new String[3][3];
+        this.board = new char[3][3];
+    }
+
+    public char getBoardCellWithCoordinates(int horizontal, int vertical) {
+        return this.board[horizontal][vertical];
+    }
+    public String getWinner() {
+        return this.winner;
     }
 }
--- TDD Tick Tac Toe/src/test/java/TicTacToeTests.java
+++ TDD Tick Tac Toe/src/test/java/TicTacToeTests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
 import java.lang.IllegalArgumentException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
@@ -17,29 +19,36 @@ public class TicTacToeTests {
 
     @Test
     public void it_should_successfully_add_move_to_board_player_one() {
+        //Act
         sut.playerOneMove(2, 0);
-        assertThat(sut.getBoard()[2][0]).isEqualTo("X");
+        //Assert
+        assertThat(sut.getBoardCellWithCoordinates(2, 0)).isEqualTo('X');
     }
 
     @Test
     public void it_should_successfully_add_move_to_board_player_two() {
+        //Act
         sut.playerTwoMove(1, 1);
-        assertThat(sut.getBoard()[1][1]).isEqualTo("O");
+        //Assert
+        assertThat(sut.getBoardCellWithCoordinates(1, 1)).isEqualTo('O');
     }
 
     @Test
     public void it_should_successfully_win_the_game_player_two() {
+        //Act
         sut.playerOneMove(2, 2);
         sut.playerTwoMove(0, 1);
         sut.playerOneMove(2, 1);
         sut.playerTwoMove(0, 2);
         sut.playerOneMove(1, 1);
         sut.playerTwoMove(0, 0);
+        //Assert
         assertThat(sut.getWinner()).isEqualTo("turan");
     }
 
     @Test
     public void it_should_successfully_must_draw_game() {
+        //Act
         sut.playerOneMove(0, 0);
         sut.playerTwoMove(1, 0);
         sut.playerOneMove(2, 0);
@@ -49,32 +58,41 @@ public class TicTacToeTests {
         sut.playerOneMove(1, 2);
         sut.playerTwoMove(0, 2);
         sut.playerOneMove(0, 1);
+        //Assert
         assertThat(sut.getWinner()).isEqualTo("draw");
     }
 
     @Test
     public void it_should_throw_exception_invalid_parameter_exception_when_wrong_coordinate() {
+        //Act
         Throwable throwable = catchThrowable(() -> sut.playerOneMove(0, 4));
+        //Assert
         assertThat(throwable).hasMessage("Invalid coordinate").isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void it_should_throw_exception_invalid_parameter_exception_when_wrong_coordinate_with_minus() {
+        //Act
         Throwable throwable = catchThrowable(() -> sut.playerOneMove(-2, -1));
+        //Assert
         assertThat(throwable).hasMessage("Invalid coordinate").isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void it_should_throw_exception_unsupported_operation_exception_when_player_plays_twice() {
+        //Act
         sut.playerOneMove(0, 1);
         Throwable throwable = catchThrowable(() -> sut.playerOneMove(0, 2));
+        //Assert
         assertThat(throwable).hasMessage("Opponent turn, please wait yours.").isInstanceOf(UnsupportedOperationException.class);
     }
 
     @Test
     public void it_should_throw_exception_unsupported_operation_exception_when_player_plays_not_empty_place() {
+        //Act
         sut.playerOneMove(0, 1);
         Throwable throwable = catchThrowable(() -> sut.playerTwoMove(0, 1));
+        //Assert
         assertThat(throwable).hasMessage("Please pick empty area.").isInstanceOf(UnsupportedOperationException.class);
     }
 
